7e5c6d179940d1f1a282ca0f61a4f3497bac571e
Abreu <rodrigodabreu@edu.unisinos.br>
2020-01-17 00:02:32 -0300

    Conceito de estado e imutabilidade

--- web/src/App.js
+++ web/src/App.js
@@ -1,18 +1,22 @@
-import React from 'react';
-import Header from './Header';
+import React, { useState } from 'react';
 
 // 3 conceitos principais
 // Componente -> Um bloco isolado de HTML, CSS e JS, o qual não interfere no restante da aplicação - Função que retorna que algum conteudo HTML
 // Propriedade ou atributo -> acessar as propriedades utilizando o props
-// Estado -> 
+// Estado -> Informações mantidas pelo componente (Imutabilidade)
 // <> fragment -> utilizado para adicionar mais de um mesmo componente
 
 function App() { 
+  const [counter, setCounter] = useState(0);
+
+  function incrementCounter(){
+    setCounter(counter + 1);
+  }
+
   return (
-    <> 
-    <Header title="Meu painel1"/>
-    <Header title="Meu painel2"/>
-    <Header title="Meu painel3"/>
+    <>
+    <h1>Contador: {counter}</h1>
+    <button onClick= {incrementCounter}>Incrementar</button>
     </>
   );
 };
